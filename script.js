8ea6247d4711a60913571a1138b5ad48145deaaa
Sunny <sunnyb67890@hotmail.com>
2022-05-28 12:28:36 -0400

    Game Logic created in function Round()
    Created the logic for a single round of RPS in the round() function.

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 
+let playerScore =0;
+let computerScore =0;
+
 function computerPlay(){
-    let choices = ["Rock", "Paper","Scisscors"];
+    let choices = ["ROCK", "PAPER","SCISSORS"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-console.log(computerPlay());
+function round(playerSelection, computerSelection){
+    playerSelection = playerSelection.toUpperCase();
+    if (playerSelection === computerSelection){
+        console.log("Tie Game");
+        return;
+    }
+
+
+    if (playerSelection =="ROCK"){
+        if(computerSelection =="PAPER"){
+            console.log("Computer Wins. Rock beats Paper");
+        }else{
+            console.log("Player Wins. Rock Beats Scissors");
+        }
+    }
+    else if (playerSelection=="SCISSORS"){
+        if(computerSelection == "PAPER"){
+            console.log("Player Wins. Scissors beats Paper");
+        }else{
+            console.log("Comp Wins. Rock beats Scissors");
+        }
+    }
+    else if (playerSelection =="PAPER"){
+        if (computerSelection == "ROCK"){
+            console.log("Player Wins. Paper beats Rock");
+        }else{
+            console.log("Comp Wins. Scissors beats Paper");
+        }
+    }
+
+}
