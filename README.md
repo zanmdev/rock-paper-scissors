5d76803fb1612e67bef272ed5309ffcdce3614bb
Sunny <sunnyb67890@hotmail.com>
2022-05-28 13:04:07 -0400

    Created a function to play RPS in a best of 5 Game
    Game() function created in the JS file that plays 5 rounds of RPS and declares a winner

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # rock-paper-scissors
+
+Introduction Javascript project to create a rock-paper-scissors game through Javascript that works in the console of the web browser. 
+
+
+Skills used: Javascript. 
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 
-let playerScore =0;
-let computerScore =0;
 
 function computerPlay(){
     let choices = ["ROCK", "PAPER","SCISSORS"];
@@ -11,30 +9,64 @@ function round(playerSelection, computerSelection){
     playerSelection = playerSelection.toUpperCase();
     if (playerSelection === computerSelection){
         console.log("Tie Game");
-        return;
+        return 0;
+        
     }
 
 
     if (playerSelection =="ROCK"){
         if(computerSelection =="PAPER"){
             console.log("Computer Wins. Rock beats Paper");
+            return -1;
         }else{
             console.log("Player Wins. Rock Beats Scissors");
+            return 1;
         }
     }
     else if (playerSelection=="SCISSORS"){
         if(computerSelection == "PAPER"){
             console.log("Player Wins. Scissors beats Paper");
+            return 1;
         }else{
             console.log("Comp Wins. Rock beats Scissors");
+            return -1;
         }
     }
     else if (playerSelection =="PAPER"){
         if (computerSelection == "ROCK"){
             console.log("Player Wins. Paper beats Rock");
+            return 1;
         }else{
             console.log("Comp Wins. Scissors beats Paper");
+            return -1;
         }
-    }
-
+    }  
 }
+
+function game(){
+    let playerScore = 0;
+    let computerScore =0;
+
+    for(let i = 0; i<5; i++){
+     let playerChoice = prompt("Choose Rock, Paper or Scissors");
+     let winner = round(playerChoice,computerPlay());
+     
+     if (winner == 1){
+         
+         playerScore++
+     }else if (winner == -1){
+        computerScore++
+     }
+
+    }
+    console.log("P:" +playerScore + " | C:" + computerScore);
+    if(playerScore > computerScore){
+        console.log("Player Won the Game");
+    }else if(playerScore < computerScore){
+        console.log("Sorry, you lost the game");
+    }else {
+        console.log("You Tied");
+    }
+    
+}
+game();
