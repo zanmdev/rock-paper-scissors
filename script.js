9708ed2e4b84f8ee93f178aef662f9c812819504
Sunny <sunnyb67890@hotmail.com>
2022-05-30 17:49:40 -0400

    Updated round function with DOM manipulation
    Changed the round function and game function to show the game being played through the page with DOM manipulation

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 
-
 function computerPlay(){
     let choices = ["ROCK", "PAPER","SCISSORS"];
     return choices[Math.floor(Math.random()*choices.length)];
 }
 
-function round(playerSelection, computerSelection){
+function round(playerSelection){
+    computerSelection = computerPlay();
     playerSelection = playerSelection.toUpperCase();
+
+    
     if (playerSelection === computerSelection){
-        console.log("Tie Game");
+        results.textContent ="Tie Game";
+        results.style.backgroundColor = "Grey";
         return 0;
         
     }
@@ -16,56 +19,79 @@ function round(playerSelection, computerSelection){
 
     if (playerSelection =="ROCK"){
         if(computerSelection =="PAPER"){
-            console.log("Computer Wins. Rock beats Paper");
+            results.textContent = "Computer Wins. Rock beats Paper";
+            results.style.backgroundColor ="Red"
+
+            
             return -1;
         }else{
-            console.log("Player Wins. Rock Beats Scissors");
+            results.textContent = "Player Wins. Rock Beats Scissors";
+            results.style.backgroundColor ="Green"
             return 1;
         }
     }
     else if (playerSelection=="SCISSORS"){
         if(computerSelection == "PAPER"){
-            console.log("Player Wins. Scissors beats Paper");
+            results.textContent ="Player Wins. Scissors beats Paper";
+            results.style.backgroundColor ="Green"
             return 1;
         }else{
-            console.log("Comp Wins. Rock beats Scissors");
+            results.textContent ="Computer Wins. Rock beats Scissors";
+            results.style.backgroundColor ="Red"
             return -1;
         }
     }
     else if (playerSelection =="PAPER"){
         if (computerSelection == "ROCK"){
-            console.log("Player Wins. Paper beats Rock");
+            results.textContent ="Player Wins. Paper beats Rock";
+            results.style.backgroundColor ="Green"
             return 1;
         }else{
-            console.log("Comp Wins. Scissors beats Paper");
+            results.textContent ="Computer Wins. Scissors beats Paper";
+            results.style.backgroundColor ="Red"
             return -1;
         }
     }  
 }
 
-function game(){
-    let playerScore = 0;
-    let computerScore =0;
-
-    
-     let playerChoice = prompt("Choose Rock, Paper or Scissors");
-     let winner = round(playerChoice,computerPlay());
+function game(winner){
+    console.log(winner);
      
      if (winner == 1){
-         
          playerScore++
+         pScore.textContent = playerScore;
      }else if (winner == -1){
         computerScore++
+        cScore.textContent = computerScore;
      }
 
-    
-    console.log("P:" +playerScore + " | C:" + computerScore);
-    if(playerScore > computerScore){
-        console.log("Player Won the Game");
-    }else if(playerScore < computerScore){
-        console.log("Sorry, you lost the game");
-    }else {
-        console.log("You Tied");    
-    }
+     if(playerScore == 5 || computerScore == 5){
+        //Stop game and call for winner
+     }
+
+
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+
+const results = document.querySelector(".result");
+
+const pScore= document.querySelector(".player-score");
+const cScore = document.querySelector(".cpu-score");
+
+rock.addEventListener("click",function(){
+    game(round("Rock"));
+});
+
+paper.addEventListener("click",function(){
+    game(round("Paper"));
+});
+
+scissors.addEventListener("click",function(){
+    game(round("Scissors"));
+});
